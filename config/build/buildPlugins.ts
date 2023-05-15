3519a06201b13d0da77b1015ed57a2b1eafba6d4
Sphaerista <cosaleks@gmail.com>
2023-05-15 19:36:26 +0300

    initial commit

---
+++ config/build/buildPlugins.ts
@@ -0,0 +1,20 @@
+import HtmlWebpackPlugin from "html-webpack-plugin";
+import MiniCssExrtactPlugin from "mini-css-extract-plugin";
+import path from "path";
+import webpack from "webpack";
+import { BuildOptions } from "./types/config";
+
+export function buildPlugins({
+  paths,
+}: BuildOptions): webpack.WebpackPluginInstance[] {
+  return [
+    new webpack.ProgressPlugin(),
+    new HtmlWebpackPlugin({
+      template: paths.html,
+    }),
+    new MiniCssExrtactPlugin({
+      filename: "css/[name].[contenthash:8].css",
+      chunkFilename: "css/[name].[contenthash:8].css",
+    }),
+  ];
+}
---
+++ src/App.tsx
@@ -0,0 +1,26 @@
+import React, { Suspense } from "react";
+import { Route, Routes } from "react-router-dom";
+import { MainPageAsync } from "./pages/MainPage/MainPage.async";
+import { AboutPageAsync } from "./pages/AboutPage/AboutPage.async";
+import { Link } from "react-router-dom";
+import "./styles/index.scss";
+import { useTheme } from "./theme/useTheme";
+import { classNames } from "./helpers/classNames/classNames";
+
+export const App = () => {
+  const { theme, toggleTheme } = useTheme();
+
+  return (
+    <div className={classNames("app", {}, [theme])}>
+      <button onClick={toggleTheme}>Change theme</button>
+      <Link to={"/about"}>About</Link>
+      <Link to={"/"}>Main</Link>
+      <Suspense fallback={<>Loading...</>}>
+        <Routes>
+          <Route path={"/about"} element={<AboutPageAsync />} />
+          <Route path={"/"} element={<MainPageAsync />} />
+        </Routes>
+      </Suspense>
+    </div>
+  );
+};
