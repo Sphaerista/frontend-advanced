e354ac637f631a0bb11d18eb890d0db1d59429e5
Sphaerista <cosaleks@gmail.com>
2023-05-19 15:41:02 +0300

    add sidebar & navbar

---
+++ src/widgets/Sidebar/ui/Sidebar/Sidebar.tsx
@@ -0,0 +1,32 @@
+import { classNames } from "shared/lib/classNames/classNames";
+import cls from "./Sidebar.module.scss";
+import { useState } from "react";
+import { ThemeSwitcher } from "widgets/ThemeSwitcher";
+import { LangSwitcher } from "widgets/LangSwitcher/LangSwitcher";
+
+interface SidebarProps {
+  className?: string;
+}
+
+export const Sidebar: React.FC<SidebarProps> = (props) => {
+  const { className } = props;
+  const [collapsed, setCollapsed] = useState(false);
+
+  const onToogle = () => {
+    setCollapsed((prev) => !prev);
+  };
+
+  return (
+    <div
+      className={classNames(cls.sidebar, { [cls.collapsed]: collapsed }, [
+        className,
+      ])}
+    >
+      <button onClick={onToogle}>clcijk</button>
+      <div className={cls.switchers}>
+        <ThemeSwitcher />
+        <LangSwitcher className={cls.lang} />
+      </div>
+    </div>
+  );
+};
