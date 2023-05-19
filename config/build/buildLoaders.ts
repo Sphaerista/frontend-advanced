0d610be94faa25cd6f4d854af52543aa8bd4cf6b
Sphaerista <cosaleks@gmail.com>
2023-05-19 15:48:56 +0300

    add babel

--- config/build/buildLoaders.ts
+++ config/build/buildLoaders.ts
@@ -3,6 +3,28 @@ import webpack from "webpack";
 import { BuildOptions } from "./types/config";
 
 export function buildLoaders({ isDev }: BuildOptions): webpack.RuleSetRule[] {
+  const babelLoader = {
+    test: /\.(js|jsx|tsx)$/,
+    exclude: /node_modules/,
+    use: {
+      loader: "babel-loader",
+      options: {
+        presets: ["@babel/preset-env"],
+      },
+    },
+  };
+  const fileLoader = {
+    test: /\.(png|jpe?g|gif|woff2|woff)$/i,
+    use: [
+      {
+        loader: "file-loader",
+      },
+    ],
+  };
+  const svgLoader = {
+    test: /\.svg$/,
+    use: ["@svgr/webpack"],
+  };
   const cssLoader = {
     test: /\.s[ac]ss$/i,
     use: [
@@ -28,5 +50,5 @@ export function buildLoaders({ isDev }: BuildOptions): webpack.RuleSetRule[] {
     use: "ts-loader",
     exclude: /node_modules/,
   };
-  return [typescriptLoader, cssLoader];
+  return [cssLoader, svgLoader, fileLoader, babelLoader, typescriptLoader];
 }
