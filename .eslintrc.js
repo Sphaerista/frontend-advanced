385242123ecf6c39d998b043e81a03e3fc7dbb21
Sphaerista <cosaleks@gmail.com>
2023-05-20 13:17:42 +0300

    add eslint

---
+++ .eslintrc.js
@@ -0,0 +1,22 @@
+module.exports = {
+  env: {
+    browser: true,
+    es2021: true,
+  },
+  extends: ["plugin:react/recommended"],
+  overrides: [],
+  parser: "@typescript-eslint/parser",
+  parserOptions: {
+    ecmaVersion: "latest",
+    sourceType: "module",
+  },
+  plugins: ["react", "@typescript-eslint"],
+  rules: {
+    "react/react-in-jsx-scope": "off",
+    "react/prop-types": "warn",
+    "react/jsx-indent": [2, { indentMode: 4, ignoreTernaryOperator: true }],
+  },
+  globals: {
+    __IS_DEV__: true,
+  },
+};
