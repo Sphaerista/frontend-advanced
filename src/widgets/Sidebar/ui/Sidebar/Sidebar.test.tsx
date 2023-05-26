0aec4ddb3e0ca1ef6ae321ca5adbc62cea6bfe66
Sphaerista <cosaleks@gmail.com>
2023-05-26 19:16:48 +0300

    fix tests

--- src/widgets/Sidebar/ui/Sidebar/Sidebar.test.tsx
+++ src/widgets/Sidebar/ui/Sidebar/Sidebar.test.tsx
@@ -1,15 +1,15 @@
 // import React from "react";
 import { fireEvent, screen } from "@testing-library/react";
 import { Sidebar } from "widgets/Sidebar/ui/Sidebar/Sidebar";
-import { renderWithTranslation } from "shared/lib/tests/renderWithTranslation";
+import { componentRender } from "shared/lib/tests/componentRender";
 
 describe("Sidebar", () => {
   test("with only one param", () => {
-    renderWithTranslation(<Sidebar />);
+    componentRender(<Sidebar />);
     expect(screen.getByTestId("sidebar")).toBeInTheDocument();
   });
   test("toggle collapsed", () => {
-    renderWithTranslation(<Sidebar />);
+    componentRender(<Sidebar />);
     const toggleBtn = screen.getByTestId("sidebar-toggle");
     fireEvent.click(toggleBtn);
     expect(screen.getByTestId("sidebar")).toHaveClass("collapsed");
