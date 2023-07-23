f308dceba2b79db7eeb60154a1f318bb6e9fe80a
Sphaerista <cosaleks@gmail.com>
2023-07-23 18:26:17 +0300

    fix tests

--- src/features/AuthByUsername/model/services/loginByUsername/loginByUsername.test.ts
+++ src/features/AuthByUsername/model/services/loginByUsername/loginByUsername.test.ts
@@ -5,9 +5,6 @@ import { StateSchema } from "app/providers/StoreProvider";
 import { userActions } from "entities/User";
 import { TestAsyncThunk } from "shared/lib/tests/testAsyncThunk";
 
-jest.mock("axios");
-const mockedAxios = jest.mocked(axios, true);
-
 describe("loginByUsername.test", () => {
   //   let dispatch: Dispatch;
   //   let getState: () => StateSchema;
@@ -42,28 +39,28 @@ describe("loginByUsername.test", () => {
 
   test("success login", async () => {
     const userValue = { username: "aa", id: "1" };
-    mockedAxios.post.mockReturnValue(Promise.resolve({ data: userValue }));
 
     const thunk = new TestAsyncThunk(loginByUsername);
+    thunk.api.post.mockReturnValue(Promise.resolve({ data: userValue }));
     const result = await thunk.callThunk({ username: "aa", password: "0000" });
 
     expect(thunk.dispatch).toHaveBeenCalledWith(
       userActions.setAuthData(userValue)
     );
     expect(thunk.dispatch).toHaveBeenCalledTimes(3);
-    expect(mockedAxios.post).toHaveBeenCalled();
+    expect(thunk.api.post).toHaveBeenCalled();
     expect(result.meta.requestStatus).toBe("fulfilled");
     expect(result.payload).toEqual(userValue);
   });
 
   test("error login", async () => {
-    mockedAxios.post.mockReturnValue(Promise.resolve({ status: 403 }));
-
     const thunk = new TestAsyncThunk(loginByUsername);
+    thunk.api.post.mockReturnValue(Promise.resolve({ status: 403 }));
+
     const result = await thunk.callThunk({ username: "aa", password: "0000" });
 
     expect(thunk.dispatch).toHaveBeenCalledTimes(2);
-    expect(mockedAxios.post).toHaveBeenCalled();
+    expect(thunk.api.post).toHaveBeenCalled();
     expect(result.meta.requestStatus).toBe("rejected");
     expect(result.payload).toBe("error");
   });
