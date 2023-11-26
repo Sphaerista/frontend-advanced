0f0f58410828debf604784c2c3c70c81492df561
Sphaerista <cosaleks@gmail.com>
2023-11-26 17:58:23 +0300

    add vite for dev mode (issue with imports)

--- src/app/providers/ThemeProvider/index.ts
+++ src/app/providers/ThemeProvider/index.ts
@@ -1,6 +1,6 @@
-import { Theme } from "./lib/ThemeContext";
-import { useTheme } from "./lib/useTheme";
-import { ThemeProvider } from "./ui/ThemeProvider";
-import { ThemeSBProvider } from "./ui/ThemeProviderDecorator";
+export { Theme } from "./lib/ThemeContext";
+export { useTheme } from "./lib/useTheme";
+export { ThemeProvider } from "./ui/ThemeProvider";
+export { ThemeSBProvider } from "./ui/ThemeProviderDecorator";
 
-export { ThemeProvider, useTheme, Theme, ThemeSBProvider };
+// export { ThemeProvider, useTheme, Theme, ThemeSBProvider };
